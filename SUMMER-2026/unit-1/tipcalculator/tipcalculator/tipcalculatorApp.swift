20ef0e8de62f2bdf79b45c5cb0010d15d5a04577
Santiago Morales <santymoravilla@gmail.com>
2026-05-12 18:56:49 -0500

    Added Summer 2026 unit-1 and updates

---
+++ SUMMER-2026/unit-1/tipcalculator/tipcalculator/tipcalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipcalculatorApp.swift
+//  tipcalculator
+//
+//  Created by ChrisVilla on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct tipcalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
